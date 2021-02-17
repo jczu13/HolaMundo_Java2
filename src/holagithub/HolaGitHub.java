cbdd37d044c0602b486333c33df1c8b0b6d9bd73
Carlos Zuniga <jczuzu13@gmail.com>
2021-02-17 11:36:49 -0600

    Se le agrego ';' faltates un comentario final

--- src/holagithub/HolaGitHub.java
+++ src/holagithub/HolaGitHub.java
@@ -5,7 +5,8 @@ public class HolaGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Adios")
+        System.out.println("Adios");
+        System.out.println("No me queda muy claro esto");
     }
     
 }
